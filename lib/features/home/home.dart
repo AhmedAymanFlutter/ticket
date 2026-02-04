98b223331790e4836e4e74ac03ac2849af2b6ccb
Mo Emad <mo.elbakarawy.1@gmail.com>
2026-02-04 14:08:52 +0200

    Added Home screen and fixed navigation

---
+++ lib/features/home/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(leading: Icon(Icons.message), title: Text('Messages')),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(leading: Icon(Icons.settings), title: Text('Settings')),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+      body: Center(child: Text('Home')),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ticket/core/helper/app_text_style.dart';
 import 'package:ticket/core/utils/app_colors.dart';
 import 'package:ticket/core/widgets/card_widgets.dart' show CustomCard;
+import 'package:ticket/features/home/home.dart';
 import 'injection_container.dart' as di;
 
 void main() async {
@@ -25,35 +26,51 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'Ticket App',
           theme: ThemeData(primarySwatch: Colors.blue),
-          home: Scaffold(
-            backgroundColor: AppColors.brandPrimary,
-            body: Center(
-              child: Flexible(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 200.h,
-                      child: ListView.builder(
-                        
-                        scrollDirection: Axis.vertical,
-                        itemCount: 10,
-                        itemBuilder: (context, index) {
-                          return const CustomCard(
-                            icon: Icon(Icons.access_alarm),
-                            title: 'Title',
-                            subtitle: 'Subtitle',
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          home: const LandingScreen(),
         );
       },
+    );
+  }
+}
+
+class LandingScreen extends StatelessWidget {
+  const LandingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const Home()),
+          );
+        },
+        child: const Text('Go to Home'),
+      ),
+      backgroundColor: AppColors.brandPrimary,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 200.h,
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                scrollDirection: Axis.vertical,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return const CustomCard(
+                    icon: Icon(Icons.access_alarm),
+                    title: 'Title',
+                    subtitle: 'Subtitle',
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
